b2638e4583d4a7c059151283fe3ab39bac8bfce8
Lotus <cmadisonne@gmail.com>
2018-07-31 14:12:28 -0500

    Classwork7 Input

---
+++ Ex17.js
@@ -0,0 +1,13 @@
+function main(){
+    var num1 = 3;
+    var num2 = 7;
+    var num3 = 11;
+    alert(threeVars(num1,num2,num3))
+}
+
+function threeVars(number1, number2, number3) {
+    var sumNum = number1 + number2 + number3;
+    return sumNum;
+}
+
+main();
